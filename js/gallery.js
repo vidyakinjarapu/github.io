c49cf20c1e99be2807cb0ce130e0352f6a8d07e6
vidya kinjarapu <vidya5594@gmail.com>
2021-03-01 22:17:35 +0530

    Get rid of delay & Finish project

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,38 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
+
 function activateGallery(){
-  alert("hello from the gallery file")
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+
+  thumbnails.forEach(function(thumbnail){
+
+    // Preload the image to get rid of delay.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener("click", function(){
+      // let newImagesrc = thumbnail.dataset.largeVersion;
+
+      // Change the main image when clicked on a thumbnail.
+      mainImage.setAttribute("src", largeVersion.src);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update the image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
